1df0aad5b5115ec42f997cda1265b4c8d2791afa
ArtemKarablinov <Artem.karablinov@raiffeisen.ru>
2020-10-11 22:04:14 +0300

    HW5 refactor

--- HW5/deploy.sh
+++ HW5/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sbt assembly
 mv target/scala-2.11/hw5-assembly-0.1.jar target/scala-2.11/hw5.jar
-scp target/scala-2.11/hw5.jar n.sviridenko@rf-cluster.dadadata.ru:hw5
+scp target/scala-2.11/hw5.jar a.karablinov@rf-cluster.dadadata.ru:hw5
 scp script.py a.karablinov@rf-cluster.dadadata.ru:hw5
