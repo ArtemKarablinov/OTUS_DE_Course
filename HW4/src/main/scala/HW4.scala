b6c2b0246f8d8748519900296e8e1423af3a8d21
ArtemKarablinov <Artem.karablinov@raiffeisen.ru>
2020-10-11 21:50:03 +0300

    HW4

--- HW4/src/main/scala/HW4.scala
+++ HW4/src/main/scala/HW4.scala
@@ -6,34 +6,34 @@ import org.apache.spark.sql.functions._
 
 object HW4 extends App {
 
-  val spark = SparkSession
-    .builder
-    .appName("WordCount")
-    .getOrCreate()
+    val spark = SparkSession
+        .builder
+        .appName("WordCount")
+        .getOrCreate()
 
 
-  val sc = spark.sparkContext
+    val sc = spark.sparkContext
 
-  import spark.implicits._
+    import spark.implicits._
 
-  val lines = spark.readStream
-    .format("socket")
-    .option("host", "localhost")
-    .option("port", 666)
-    .load()
+    val lines = spark.readStream
+        .format("socket")
+        .option("host", "localhost")
+        .option("port", 666)
+        .load()
 
-  // Split the lines into words
-  val words = lines.as[String].flatMap(_.split(" "))
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
 
-  // Generate running word count
-  val wordCounts = words.groupBy("value").count()
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
 
-  val query = wordCounts.writeStream
-    .outputMode("complete")
-    .format("console")
-    .option("checkpointLocation", "HW4/checkpoint")
-    .start()
+    val query = wordCounts.writeStream
+        .outputMode("complete")
+        .format("console")
+        .option("checkpointLocation", "HW4/checkpoint")
+        .start()
 
-  query.awaitTermination()
+    query.awaitTermination()
 
 }
